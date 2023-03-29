441dfab559a1692036cba6da2578cfa9215a43fb
Konul304 <memmedovakonul270@gmail.com>
2023-03-29 16:49:49 +0400

    feat: routing between pages

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Create from './components/create'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Read from './components/read';
+import Update from './components/update';
 
 function App() {
   return (
-    <div className="container_main">
-      <div className="form_container text-light text-center p-5 fs-3 fw-bold">
-        <p className="mb-5">React CRUD Operations</p>
-      <Create/>
+    <Router>
+      <div className="container_main">
+        <div className="form_container text-light text-center p-5 fs-3 fw-bold">
+          <p className="mb-5">React CRUD Operations</p>
+          <Routes>
+            <Route path='/create' element={<Create />} />
+            <Route path='/read' element={<Read />} />
+            <Route path='/update' element={<Update />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
