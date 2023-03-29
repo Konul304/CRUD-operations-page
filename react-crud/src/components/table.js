b13d35de74860fe4b44343068b704a337ca4fd43
Konul304 <memmedovakonul270@gmail.com>
2023-03-29 17:28:22 +0400

    get selected row data from API

--- react-crud/src/components/table.js
+++ react-crud/src/components/table.js
@@ -1,6 +1,7 @@
 import '../App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function Table() {
     const [APIData, setAPIData] = useState([]);
@@ -12,14 +13,20 @@ export default function Table() {
             })
     }, []);
 
+    const setData = (data) =>{
+        console.log(data)
+    }
+
     return (
         <>
+        <div className='table_container '>
             <table className="table bg-light">
                 <thead>
                     <tr>
                         <th scope="col">First Name</th>
                         <th scope="col">Last Name</th>
                         <th scope="col">Checked</th>
+                        <th scope="col">Update</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,11 +36,15 @@ export default function Table() {
                                 <td>{data.firstName}</td>
                                 <td>{data.lastName}</td>
                                 <td>{data.checkbox ? 'Checked' : 'Unchecked'}</td>
+                                <td>
+                                    <Link to='/update'><button onClick={() => setData(data)} className='btn btn-primary'>Update</button></Link>
+                                </td>
                             </tr>
                         )
                     })}
                 </tbody>
             </table>
+            </div>
         </>
     )
 }
